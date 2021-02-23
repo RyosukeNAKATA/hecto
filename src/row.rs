ee14ec708169ee5825ed1066a7074efe1c370feb
RyosukeNAKATA <ryosuke.nakata.2019@ds.musashino-u.ac.jp>
2021-02-23 17:18:38 +0900

    impl highlight number

--- src/row.rs
+++ src/row.rs
@@ -26,6 +26,7 @@ impl Row {
         let end = cmp::min(end, self.string.len());
         let start = cmp::min(start, end);
         let mut result = String::new();
+        let mut current_highlighting = &highlighting::Type::None;
         #[allow(clippy::integer_arithmetic)]
         for (index, grapheme) in self.string[..]
             .graphemes(true)
@@ -38,18 +39,20 @@ impl Row {
                     .highlighting
                     .get(index)
                     .unwrap_or(&highlighting::Type::None);
-                let start_highlight =
-                    format!("{}", termion::color::Fg(highlighting_type.to_color()));
-                result.push_str(&start_highlight[..]);
+                if highlighting_type != current_highlighting {
+                    current_highlighting = highlighting_type;
+                    let start_highlight =
+                        format!("{}", termion::color::Fg(highlighting_type.to_color()));
+                }
                 if c == '\t' {
                     result.push_str(" ");
                 } else {
                     result.push(c);
                 }
-                let end_highlight = format!("{}", termion::color::Fg(color::Reset));
-                result.push_str(&end_highlight[..]);
             }
         }
+        let end_highlight = format!("{}", termion::color::Fg(color::Reset));
+        result.push_str(&end_highlight[..]);
         result
     }
     pub fn len(&self) -> usize {
@@ -123,7 +126,7 @@ impl Row {
         self.string.as_bytes()
     }
     pub fn find(&self, query: &str, at: usize, direction: SearchDirection) -> Option<usize> {
-        if at > self.len {
+        if at > self.len || query.is_empty() {
             return None;
         }
         let start = if direction == SearchDirection::Forward {
@@ -159,14 +162,54 @@ impl Row {
         }
         None
     }
-    pub fn highlight(&mut self) {
+    pub fn highlight(&mut self, word: Option<&str>) {
         let mut highlighting = Vec::new();
-        for c in self.string.chars() {
-            if c.is_ascii_digit() {
+        let chars: Vec<char> = self.string.chars().collect();
+        let mut matches = Vec::new();
+        let mut search_index = 0;
+
+        if let Some(word) = word {
+            while let Some(search_match) = self.find(word, search_index, SearchDirection::Forward) {
+                matches.push(search_match);
+                if let Some(next_index) = search_match.checked_add(word[..].graphemes(true).count())
+                {
+                    search_index = next_index;
+                } else {
+                    break;
+                }
+            }
+        }
+
+        let mut prev_is_separator = true;
+        let mut index = 0;
+        while let Some(c) = chars.get(index) {
+            if let Some(word) = word {
+                if matches.contains(&index) {
+                    for _ in word[..].graphemes(true) {
+                        index += 1;
+                        highlighting.push(highlighting::Type::Match);
+                    }
+                    continue;
+                }
+            }
+
+            let previous_highlight = if index > 0 {
+                #[allow(clippy::integer_arithmetic)]
+                highlighting
+                    .get(index - 1)
+                    .unwrap_or(&highlighting::Type::None)
+            } else {
+                &highlighting::Type::None
+            };
+            if c.is_ascii_digit()
+                && (prev_is_separator || previous_highlight == &highlighting::Type::Number)
+                || (c == &'.' && previous_highlight == &highlighting::Type::Number)
+            {
                 highlighting.push(highlighting::Type::Number);
             } else {
                 highlighting.push(highlighting::Type::None);
             }
+            index += 1;
         }
         self.highlighting = highlighting;
     }
